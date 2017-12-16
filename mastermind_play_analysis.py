a9dedeed9dc0c1f44ca960ae8ed751f539af8301
Hector Vazquez <hjvm@dhcp-18-189-44-225.dyn.mit.edu>
2017-12-15 23:50:25 -0500

    Added Complex AI player

--- mastermind_play_analysis.py
+++ mastermind_play_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import mastermind_prob as game
-import mastermind_ai_play as ai
+import mastermind_ai_play as simple_ai
 import matplotlib.pyplot as plt
 import numpy as np
 from ast import literal_eval
@@ -20,11 +20,14 @@ for index, row in data.iterrows():
     sol = row.Solution.strip()
     sequences = row.Sequences.strip('[ ]').split(', ')
     player_prob = [] # Probability player has enough info to guess the correct sequence
-    ai_prob = [] # Same as above but for ai bot
+    simple_ai_prob = [] # Same as above but for simple_ai bot
+    complex_ai_prob = [] # Same as above but for complex_ai bot
     player_round = game.Round(sol, dist, verbose=False)
-    ai_round = game.Round(sol, dist, verbose=False)
+    simple_ai_round = game.Round(sol, dist, verbose=False)
+    complex_ai_round = game.Round(sol, dist, verbose=False)
     player_prob.append(player_round.solution_prediction[sol])
-    ai_prob.append(ai_round.solution_prediction[sol])
+    simple_ai_prob.append(simple_ai_round.solution_prediction[sol])
+    complex_ai_prob.append(complex_ai_round.solution_prediction[sol])
 
     print('+++++++++++++++++++++++++')
     print('Player:',row.Participant)
@@ -36,55 +39,100 @@ for index, row in data.iterrows():
         player_round.next_turn(seq)
         player_prob.append(player_round.solution_prediction[sol])
         print(player_round.solution_prediction[sol])
+    player_turns = len(sequences)
     print('Number of turns:', len(sequences))
+    
     print('---------------')
-    print('Mastermind AI:\n')
-    #print(ai_round.solution_prediction[sol])
-    ai_player = ai.AIPlay(dist)
+    
+    print('Simple AI:\n')
+    simple_ai_player = simple_ai.AIPlay(dist)
     out = (0,0)
     print("Solution:", sol)
-    turns = 0
-    ai_guesses = [0.0]
+    simple_ai_turns = 0
+    simple_ai_guesses = [0.0]
     while (out[1] != 3):
-        seq = ai_player.play_turn(out)
-        ai_guesses.append(ai_round.solution_prediction[seq])
-        turns += 1
+        seq = simple_ai_player.play_turn(out)
+        simple_ai_guesses.append(simple_ai_round.solution_prediction[seq])
+        simple_ai_turns += 1
         print("Chosen sequence:",seq)
-        ai_round.next_turn(seq)
+        simple_ai_round.next_turn(seq)
         out = game.observe_sequence(seq, sol)
         print("Feedback:",out)
-        ai_prob.append(ai_round.solution_prediction[sol])
-        print(ai_round.solution_prediction[sol])
-    print('Number of turns:', turns)
+        simple_ai_prob.append(simple_ai_round.solution_prediction[sol])
+        print(simple_ai_round.solution_prediction[sol])
+    print('Number of turns:', simple_ai_turns)
     
+    print('------------------')
+
+    print('Complex AI:\n')
+    complex_ai_player = game.ComplexAI(dist)
+    out = (0,0)
+    print("Solution:", sol)
+    complex_ai_turns = 0
+    complex_ai_guesses = [0.0]
+    while (out[1] != 3):
+        seq = complex_ai_player.get_sequence()
+        complex_ai_guesses.append(complex_ai_round.solution_prediction[seq])
+        complex_ai_turns += 1
+        print("Chosen sequence:",seq)
+        complex_ai_round.next_turn(seq)
+        out = game.observe_sequence(seq, sol)
+        print("Feedback:",out)
+        complex_ai_player.update_belief_state(seq, out)
+        complex_ai_prob.append(complex_ai_round.solution_prediction[sol])
+        print(complex_ai_round.solution_prediction[sol])
+    print('Number of turns:', complex_ai_turns)
     # Plot performance of AI vs Human Player
-    x = np.arange(max(len(sequences), turns) + 1)
+    x = np.arange(max((simple_ai_turns, player_turns, complex_ai_turns)) + 1)
     
-    ai_color = 'red'
+    simple_ai_color = 'red'
+    complex_ai_color = 'purple'
     player_color = 'blue'
-    if turns < len(sequences):
-        win_color = ai_color
-        vline = turns
-    elif len(sequences) < turns:
+    
+    colors = np.array([player_color, simple_ai_color, complex_ai_color])
+    turns = np.array([player_turns, simple_ai_turns, complex_ai_turns])
+    guesses = [player_guesses, simple_ai_guesses, complex_ai_guesses]
+    probs = [player_prob, simple_ai_prob, complex_ai_prob]
+
+    win_color = colors[np.argmin(turns)]
+    vline = np.min(turns)
+    
+    '''
+    if simple_ai_turns < player_turns:
+        win_color = simple_ai_color
+        vline = simple_ai_turns
+    elif player_turns < simple_ai_turns:
         win_color = player_color
         vline = len(sequences)
     else:
         win_color = 'green'
         vline = turns
+    '''
 
-    while not (len(ai_prob) == len(player_prob)):
-        if len(ai_prob) < len(player_prob):
-            ai_prob.append(1.0)
+    for prob in probs:
+        prob.extend([1.0 for x in range(np.max(turns) + 1 - len(prob))])
+    for guess in guesses:
+        guess.extend([guess[-1] for x in range(np.max(turns) +1 - len(guess))])
+
+    '''
+    while not (len(simple_ai_prob) == len(player_prob)):
+        if len(simple_ai_prob) < len(player_prob):
+            simple_ai_prob.append(1.0)
         else:
             player_prob.append(1.0)
+    '''
+
+    x = np.arange(max(turns) + 1)
 
     # Graph 'Percieved Probability' of Correct Solution
     plt.subplot(211)
-    plt.plot(x, player_prob, color=player_color)
-    plt.plot(x, ai_prob, color=ai_color)
+    plt.plot(x, probs[0], color=colors[0])
+    plt.plot(x, probs[1], color=colors[1])
+    plt.plot(x, probs[2], color=colors[2])
     plt.axvline(x=vline, color=win_color,linestyle='dashed')
     
-    plt.legend(['Player probability', 'AI probability', ('Solution: ' + sol)], loc='upper left')
+    plt.legend(['Player probability', 'Simple AI probability', 'Complex AI probability', \
+                ('Solution: ' + sol)], loc='upper left')
     plot_title = row.Participant + ' -  Distribution: ' + str(int(row.Distribution)) \
                                  + ' - Round: ' + str(int(row.Round))
     plt.title('P(S=solution|Observations)', fontsize=10)
@@ -94,14 +142,17 @@ for index, row in data.iterrows():
 
     # Graph Probability of Correctness of current sequence being played
     plt.subplot(212)
-
-    while not (len(ai_guesses) == len(player_guesses)):
-        if len(ai_guesses) < len(player_guesses):
-            ai_guesses.append(ai_guesses[-1])
+    
+    '''
+    while not (len(simple_ai_guesses) == len(player_guesses)):
+        if len(simple_ai_guesses) < len(player_guesses):
+            simple_ai_guesses.append(simple_ai_guesses[-1])
         else:
             player_guesses.append(player_guesses[-1])
-    plt.plot(x, player_guesses, color=player_color)
-    plt.plot(x, ai_guesses, color=ai_color)
+    '''
+    plt.plot(x, guesses[0], color=colors[0])
+    plt.plot(x, guesses[1], color=colors[1])
+    plt.plot(x, guesses[2], color=colors[2])
     plt.axvline(x=vline, color=win_color,linestyle='dashed')
     
     plt.title('P(S=guess|Observations)',fontsize=10)
